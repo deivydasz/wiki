4286fc93144d86d9aa92840f79c27b5d4de29ce4
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-08-21 19:45:33 +0300

    Switch database to postgresql

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:ecuSinner/wiki.git'
 
 set :passenger_restart_with_touch, true
 
-set :deploy_to, '/home/deivis/notes'
+set :deploy_to, '/home/deivis/wiki'
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
